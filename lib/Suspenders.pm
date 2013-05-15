587dcdb5b014cd509fbe80ddcbf4210e4d35d4de
tokuhirom <tokuhirom@gmail.com>
2013-05-15 22:49:25 +0900

    refactoring

--- lib/Suspenders.pm
+++ lib/Suspenders.pm
@@ -7,17 +7,26 @@ package Suspenders::Backend::Exec;
 
 sub new { bless {}, shift }
 
+sub as_string { "exec" }
+
 sub run {
-    my $cmd = $Suspenders::COMMAND
-        or die "Invalid sequence";
-    my $code = Suspenders::Commands->can($cmd)
-        or die "Unknown command: '$cmd'";
-    my $cmdline = $code->($Suspenders::STUFF, @Suspenders::ARGS);
-    my $retval = system($cmdline);
-    my $succeeded = $Suspenders::NOT ? $retval != 0 : $retval == 0;
-    my $msg = join(' ', @Suspenders::MSG);
-    printf("    %s %s\n", $succeeded ? 'o' : 'x', $msg);
-    $Suspenders::FAILED++ unless $succeeded;
+    my ($self, $cmdline) = @_;
+    return system($cmdline);
+}
+
+package Suspenders::Backend::SSH;
+
+sub new {
+    my ($class, $host) = @_;
+    Carp::croak("Missing mandatory parameter: host") unless $host;
+    bless { host => $host }, $class;
+}
+
+sub as_string { "ssh @{[ $_[0]->{host} ]}" }
+
+sub run {
+    my ($self, $cmdline) = @_;
+    return system('ssh', $self->{host}, $cmdline);
 }
 
 package Suspenders::Commands;
@@ -27,6 +36,7 @@ sub check_file {
     # "test -f %1"
     "test -f @{[ shell_quote shift ]}";
 }
+
 sub check_file_contain {
     sprintf('grep -q %s %s',
         quotemeta $_[1],
@@ -39,7 +49,7 @@ use parent qw(Exporter);
 
 our $VERSION = "0.01";
 
-our @EXPORT = qw( it should be file describe not contain );
+our @EXPORT = qw( it should be file describe not contain backend_ssh );
 
 our $STUFF;
 our $NOT;
@@ -48,13 +58,24 @@ our @ARGS;
 our @MSG;
 our $FAILED;
 our $BACKEND = Suspenders::Backend::Exec->new();
+our $BANNER;
+
 BEGIN { $|++ };
+
+sub backend_ssh($) {
+    $BACKEND = Suspenders::Backend::SSH->new(@_);
+}
 
 sub describe {
     my ($stuff, $code) = @_;
     local $STUFF = $stuff;
     local $NOT = 0;
     local $COMMAND;
+
+    unless ($BANNER++) {
+        printf "  Remote:%s\n\n", $BACKEND->as_string();
+    }
+
     printf("  $stuff\n");
     $code->();
     print("\n");
@@ -71,7 +92,20 @@ sub END {
 
 sub it {
     unshift @MSG, 'it';
-    $Suspenders::BACKEND->run();
+
+    my $cmd = $Suspenders::COMMAND
+        or die "Invalid sequence";
+    my $code = Suspenders::Commands->can($cmd)
+        or die "Unknown command: '$cmd'";
+    my $cmdline = $code->($Suspenders::STUFF, @Suspenders::ARGS);
+
+    my $retval = $Suspenders::BACKEND->run($cmdline);
+
+    my $succeeded = $Suspenders::NOT ? $retval != 0 : $retval == 0;
+    my $msg = join(' ', @Suspenders::MSG);
+    printf("    %s %s\n", $succeeded ? 'o' : 'x', $msg);
+    $Suspenders::FAILED++ unless $succeeded;
+
     @MSG = ();
     $NOT = 0;
 }
@@ -84,15 +118,6 @@ sub not($) { $NOT++; unshift @MSG, 'not'; }
 
 sub contain($) {unshift @MSG, 'contain', @_; $COMMAND = 'check_file_contain'; @ARGS = @_; }
 sub file()     {unshift @MSG, 'file', @_; $COMMAND = 'check_file'; @ARGS = @_; }
-
-package Suspenders::Util {
-    sub slurp {
-        my $fname = shift;
-        open my $fh, '<', $fname
-            or die "Cannot open $fname for reading: $!";
-        do { local $/; <$fh> };
-    }
-}
 
 1;
 __END__
